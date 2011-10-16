9e0c01aa789ef1d00a50a8ed20e2211087617390
Stefano Rivera <stefano@rivera.za.net>
2011-10-16 13:36:46 +0200

    Release info

--- ubuntu-activity.py
+++ ubuntu-activity.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
+import datetime
 import json
+
+from launchpadlib.launchpad import Launchpad
 
 import psycopg2
 import psycopg2.extensions
@@ -22,6 +25,8 @@ class AttrDict(dict):
 
 
 def js_date(date):
+    if date is None:
+        return None
     return int(date.strftime('%s')) * 1000
 
 
@@ -51,8 +56,39 @@ def mine_upload_history():
     return data
 
 
+def release_schedule():
+    lp = Launchpad.login_anonymously('ubuntu-activity', 'production')
+    ubu = lp.distributions['ubuntu']
+    opened = None
+    releases = []
+    freezes = {
+        'hardy': '2008-02-14',
+        'intrepid': '2008-08-28',
+        'jaunty': '2009-02-19',
+        'karmic': '2009-08-27',
+        'lucid': '2010-02-18',
+        'maverick': '2010-08-12',
+        'natty': '2011-02-24',
+        'oneiric': '2011-08-11',
+        'precise': '2011-02-16',
+    }
+    for series in reversed(list(ubu.series)):
+        freeze = freezes.get(series.name, None)
+        if freeze:
+            freeze = datetime.datetime.strptime(freeze, '%Y-%m-%d')
+        releases.append({
+            'name': series.name,
+            'opened': js_date(opened),
+            'released': js_date(series.datereleased),
+            'freeze': js_date(freeze)
+        })
+        opened = series.datereleased
+    return releases
+
+
 def main():
     data = mine_upload_history()
+    data['releases'] = release_schedule()
 
     with open('data.json', 'w') as f:
         json.dump(data, f)
